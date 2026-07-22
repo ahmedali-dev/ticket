46fda8b399bc2ff48d2eafb86a29559a2a48b246
خالد <khald@iMac-khald.local>
2026-07-22 21:36:24 +0300

    first commit

--- resources/views/ticket/dashborad.blade.php
+++ resources/views/ticket/dashborad.blade.php
@@ -3,7 +3,19 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("message.welcome") }}
+
+                    <div>
+                        <div>
+                            <form action="" method="get">
+                                @csrf
+                                <div>
+                                    <input type="text" name="search" id="search" placeholader="Search for ticket">
+
+                                    <button></button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
